4c7016193fb4b6b38199b4eb331270ce2c2e8f18
Adrien Lamoureux <adrien.lamoureux@calmid.com>
2020-07-10 13:27:11 +0900

    Fix dev main

--- src/dev_main.go
+++ src/dev_main.go
@@ -3,8 +3,14 @@
 package main
 
 import (
+	"context"
+
+	"bitbucket.org/calmisland/go-server-auth/authmiddlewares"
 	"bitbucket.org/calmisland/go-server-configs/configs"
+	"bitbucket.org/calmisland/go-server-requests/apirequests"
+	"bitbucket.org/calmisland/go-server-requests/apirouter"
 	"bitbucket.org/calmisland/go-server-requests/apiservers/httpserver"
+	"bitbucket.org/calmisland/payment-lambda-funcs/src/globals"
 	"bitbucket.org/calmisland/payment-lambda-funcs/src/handlers"
 	"bitbucket.org/calmisland/payment-lambda-funcs/src/setup/globalsetup"
 )
@@ -28,4 +34,25 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func initLambdaDevFunctions(rootRouter *apirouter.Router) {
+	authMiddleware := authmiddlewares.ValidateSession(globals.AccessTokenValidator, true)
+	devRouter := apirouter.NewRouter()
+	devRouter.AddMiddleware(authMiddleware) // Validates the user session
+
+	devRouter.AddMethodHandler("GET", "createtables", createTablesRequest)
+
+	rootRouter.AddRouter("dev", devRouter)
+}
+
+func createTablesRequest(ctx context.Context, req *apirequests.Request, resp *apirequests.Response) error {
+	err := globals.AccountDatabase.CreateDatabaseTables()
+	if err != nil {
+		return resp.SetServerError(err)
+	}
+
+	resp.SetStatus(200)
+
+	return nil
 }
